effe6f4a3acde1008bbdba3e7cbc1b6e327805de
AnnMurr <113177152+AnnMurr@users.noreply.github.com>
2023-03-24 15:08:00 +0200

    did hard work

--- Lesson-15/core/index.js
+++ Lesson-15/core/index.js
@@ -57,3 +57,24 @@ setTimeout(() => {
 }, 2000)
 
 
+// ADVANCED level
+// Task 1
+
+let firstVar = 4;
+let secondVar = 3;
+
+[firstVar, secondVar] = [secondVar, firstVar];
+console.log(firstVar);
+console.log(secondVar);
+
+
+// Task 2
+
+let codeWord1 = "обернись";
+let codeWord2 = "неужели";
+let codeWord3 = "огурцы";
+let codeWord4 = "липкие";
+let codeWord5 = "?!";
+
+let cipher = codeWord1[1] + codeWord2[1] + codeWord3[1] + codeWord4[1] + codeWord5[1];
+console.log(cipher)
